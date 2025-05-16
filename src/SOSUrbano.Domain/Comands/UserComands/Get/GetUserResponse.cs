384aa648f1bd6120867f4624a1ac2437a8f23d6a
Rafael <rafaeltabolka@hotmail.com>
2025-05-16 03:11:11 -0300

    Adicionado comandos e rotas de getById, getall e delete para usuário

---
+++ src/SOSUrbano.Domain/Comands/UserComands/Get/GetUserResponse.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SOSUrbano.Domain.Entities.UserEntity;
+
+namespace SOSUrbano.Domain.Comands.UserComands.Get
+{
+    public class GetUserResponse(User user)
+    {
+        public User User { get; set; } = user;
+    }
+}
--- src/SOSUrbano.WebApi/Controllers/UserController.cs
+++ src/SOSUrbano.WebApi/Controllers/UserController.cs
@@ -2,6 +2,9 @@
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using SOSUrbano.Domain.Comands.UserComands.Create;
+using SOSUrbano.Domain.Comands.UserComands.Delete;
+using SOSUrbano.Domain.Comands.UserComands.Get;
+using SOSUrbano.Domain.Comands.UserComands.List;
 
 namespace SOSUrbano.WebApi.Controllers
 {
@@ -9,12 +12,36 @@ namespace SOSUrbano.WebApi.Controllers
     [ApiController]
     public class UserController(ISender mediator) : ControllerBase
     {
+        [HttpGet("getAllUsers")]
+        public async Task<IActionResult> GetAllUsers()
+        {
+            var request = new ListUserRequest();
+            var response = await mediator.Send(request);
+            return Ok(response);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetUserById(Guid id)
+        {
+            var request = new GetUserRequest(id);
+            var response = await mediator.Send(request);
+            return Ok(response);
+        }
+
         [HttpPost("createUser")]
         public async Task<IActionResult> CreateUser(CreateUserRequest request)
         {
             var response = await mediator.Send(request);
             
             return Created("Created", $"{response.Message} - {response.Id}");
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteUser(Guid id)
+        {
+            var request = new DeleteUserRequest(id);
+            var response = await mediator.Send(request);
+            return Ok(response.Message);
         }
     }
 }
